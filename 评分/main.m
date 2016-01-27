25a849cf73e08c7ff0bdd06203a7f6ea3ed6e9d8
ysj <ysj@ysjdemacbook-pro.local>
2016-01-27 18:50:18 +0800

    Initial Commit

---
+++ 评分/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  评分
+//
+//  Created by ysj on 16/1/27.
+//  Copyright © 2016年 newbieYin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
